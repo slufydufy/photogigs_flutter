310a2f97993e1b4cb538db8952607898349dd58d
malfenro <malfenro@JKTOSXNB024.local>
2019-02-19 18:41:24 +0700

    ktr190219

--- lib/ProfileMain.dart
+++ lib/ProfileMain.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ProfileMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return new Scaffold(
         appBar: AppBar(
           title: new Text('Profile Main'),
@@ -11,30 +12,34 @@ class ProfileMain extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             new Container(
-                // height: MediaQuery.of(context).size.height * 0.3,
-                // padding: EdgeInsets.only(left:32.0, right: 32.0, top: 16.0),
+                
                 child: Card(
                     clipBehavior: Clip.antiAlias,
                     child: new Image.network(
                       'https://firebasestorage.googleapis.com/v0/b/photogigs-79bd9.appspot.com/o/postsImage%2F04c1de9f-808c-41c9-9cc8-deaa6a4603f2?alt=media&token=4fafbfe8-91f8-43b7-9787-f90c1c585a49',
-                      width: 100.0,
-                      // height: 100.0,
+
+                      height: height*0.4,
                       fit: BoxFit.cover,
                     ))),
                     new Container(
-                      padding: EdgeInsets.only(left: 16.0, right: 16.0),
                       child: new Card(
                         child: new Column(
                           children: <Widget>[
-                            new Text('Jeff Rozok', style: TextStyle(
+                            Container(
+                              padding: EdgeInsets.only(top: 8.0),
+                              child: new Text('Jeff Rozok', style: TextStyle(
                           fontSize: 30.0,
                           fontWeight: FontWeight.w600
                       ),),
-                      new Text('Ciseeng Jabar', style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w100,
-                          color: Colors.grey
-                      ),),
+                            ),
+                      Container(
+                        padding: EdgeInsets.only(bottom: 8.0),
+                        child: new Text('Ciseeng Jabar', style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w100,
+                            color: Colors.grey
+                        ),),
+                      ),
                           ],
                         ),
                       )
